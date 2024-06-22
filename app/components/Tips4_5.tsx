b3c5d347c0703aeb447787eb278425c9a41af848
Shin-sibainu <shincode0712@gmail.com>
2024-06-22 12:14:39 +0900

    mod

---
+++ app/components/Tips4_5.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Tips4_5 = () => {
+  return (
+    <div>
+      {/* <div className="sample_css">Subscription</div> */}
+
+      <div>
+        {/* <div className="w-20 h-10 rounded-lg shadow-[0_0_10px_theme('colors.purple.700')]"></div> */}
+        <div className="w-20 h-10 rounded-lg shadow-neon"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Tips4_5;
