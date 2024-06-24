82ad8505151c2644d3e4d8396be3b4e4a81dbb33
Shin-sibainu <shincode0712@gmail.com>
2024-06-24 15:59:08 +0900

    mod

---
+++ app/components/Button.tsx
@@ -0,0 +1,21 @@
+import { ButtonHTMLAttributes, ReactNode } from "react";
+import { twMerge } from "tailwind-merge";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: ReactNode;
+}
+
+const Button = ({ children, className }: ButtonProps) => {
+  return (
+    <div
+      className={twMerge(
+        `bg-sky-500 inline-block px-6 py-4 rounded-md text-white shadow cursor-pointer font-medium`,
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Button;
---
+++ app/components/Tips6.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Button from "./Button";
+
+const Tips6 = () => {
+  return (
+    <div className="flex gap-3">
+      <Button className="bg-red-500">Button</Button>
+      <Button className="bg-green-500">Button</Button>
+      <Button>Button</Button>
+    </div>
+  );
+};
+
+export default Tips6;
