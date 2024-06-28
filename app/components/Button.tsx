3692a24808f3a9bb0c853dde03950a7b1ef72dc2
Shin-sibainu <shincode0712@gmail.com>
2024-06-28 11:30:20 +0900

    done

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -2,25 +2,61 @@ import clsx from "clsx";
 import { ButtonHTMLAttributes, ReactNode, useState } from "react";
 import { twMerge } from "tailwind-merge";
 import { cn } from "../lib/utils";
+import { VariantProps, cva } from "class-variance-authority";
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+interface ButtonProps
+  extends ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
   children: ReactNode;
-  pending?: boolean;
 }
 
-const Button = ({ children, className, pending }: ButtonProps) => {
+const buttonVariants = cva(
+  "inline-block rounded-md text-white shadow cursor-pointer font-medium",
+  {
+    variants: {
+      variant: {
+        primary: "bg-sky-500 hover:bg-sky-600",
+        secondary: "bg-slate-500 hover:bg-slate-600",
+        danger: "bg-red-500 hover:bg-red-600",
+      },
+      size: {
+        sm: "px-4 py-2 text-sm",
+        md: "px-6 py-3",
+        lg: "px-8 py-4 text-lg",
+      },
+    },
+    defaultVariants: {
+      variant: "primary",
+      size: "md",
+    },
+  }
+);
+
+console.log(buttonVariants({ size: "lg" }));
+console.log(buttonVariants({ size: "sm" }));
+console.log(buttonVariants({ size: "md" }));
+
+const Button = ({
+  children,
+  className,
+  variant,
+  size,
+  ...props
+}: ButtonProps) => {
   return (
-    <div
-      className={cn(
-        `bg-sky-500 inline-block px-6 py-4 rounded-md text-white shadow cursor-pointer font-medium`,
-        className,
-        {
-          "bg-slate-600": pending,
-        }
-      )}
+    <button
+      // className={cn(
+      //   `bg-sky-500 inline-block px-6 py-4 rounded-md text-white shadow cursor-pointer font-medium`,
+      //   className,
+      //   {
+      //     "bg-slate-600": pending,
+      //   }
+      // )}
+      className={cn(buttonVariants({ variant, size }), className)}
+      {...props}
     >
       {children}
-    </div>
+    </button>
   );
 };
 
--- app/components/Tips7.tsx
+++ app/components/Tips7.tsx
@@ -5,9 +5,13 @@ const Tips7 = () => {
   return (
     <div className="flex gap-3">
       <Button className="bg-red-500">Button</Button>
-      <Button className="bg-green-500">Button</Button>
-      <Button>Button</Button>
-      <Button pending>Button</Button>
+      <Button className="bg-green-500" size={"sm"}>
+        Button
+      </Button>
+      <Button size={"md"}>Button</Button>
+      <Button variant={"secondary"} size={"lg"}>
+        Button
+      </Button>
     </div>
   );
 };
